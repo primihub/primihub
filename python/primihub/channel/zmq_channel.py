b68b7d6c33b274d9ee599b4943640aabd111c190
李志威 <lzw9560@gmail.com>
2022-06-26 13:05:37 +0800

    [fix]

--- python/primihub/channel/zmq_channel.py
+++ python/primihub/channel/zmq_channel.py
@@ -1,6 +1,7 @@
 import pickle
 
 import zmq
+import zmq.asyncio
 
 
 class IOService:
@@ -15,14 +16,20 @@ class Channel:
         self.socket = session.socket
 
     def send(self, data):
-        print("send message: ", data)
-        if data is not None:
+        # print("send message: ", data)
+        if data != None:
             self.socket.send(pickle.dumps(data))
 
     def recv(self):
-        print("wait for recv ...")
+        # print("wait for recv ...")
         message = self.socket.recv()
         return pickle.loads(message)
+
+    def send_json(self, data):
+        self.socket.send_json(data)
+
+    def recv_json(self):
+        return self.socket.recv_json()
 
 
 class Session:
@@ -34,11 +41,24 @@ class Session:
 
     def addChannel(self, *args) -> Channel:
         context = zmq.Context()
+        ctx_async = zmq.asyncio.Context()
+        # rep
         if self.session_mode == "server":
             socket = context.socket(zmq.REP)
             socket.bind("tcp://{}:{}".format(self.ip, self.port))
+        # req
         elif self.session_mode == "client":
             socket = context.socket(zmq.REQ)
             socket.connect("tcp://{}:{}".format(self.ip, self.port))
+
+        # push
+        elif self.session_mode == "producer":
+            socket = context.socket(zmq.PUSH)
+            socket.connect("tcp://{}:{}".format(self.ip, self.port))
+        # pull
+        elif self.session_mode == "consumer":
+            socket = ctx_async.socket(zmq.PULL)
+            socket.bind("tcp://{}:{}".format(self.ip, self.port))
+
         self.socket = socket
         return Channel(self)
