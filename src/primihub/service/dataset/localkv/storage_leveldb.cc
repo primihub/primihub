cbee1a2e8a59ed13997d879ce739f05af0b027aa
kai-think-peach <68848004+kai-think-peach@users.noreply.github.com>
2022-06-27 10:40:38 +0800

    Update storage_leveldb.cc
    Add storage_leveldb.cc,Implements the putValue、getValue、erase function.

--- src/primihub/service/dataset/localkv/storage_leveldb.cc
+++ src/primihub/service/dataset/localkv/storage_leveldb.cc
@@ -9,17 +9,26 @@
   namespace primihub::service {
   
   outcome::result<void> StorageBackendLevelDB::putValue(Key key, Value value) {
-   // TODO
+    db_->Put(leveldb::WriteOptions(), key, value);
     return outcome::success();
   }
 
   outcome::result<Value> StorageBackendLevelDB::getValue(const Key &key) const {
-   // TODO
+	Value value="";
+    leveldb::Iterator* it = db->NewIterator(leveldb::ReadOptions());
+    for (it->SeekToFirst(); it->Valid(); it->Next()) {
+		if(it->key()==key)
+		{
+			Value value=it->value();
+			break;
+		}
+    }
+    db_->Get(leveldb::ReadOptions(), key, &value)
     return Error::VALUE_NOT_FOUND;
   }
 
   outcome::result<void> StorageBackendLevelDB::erase(const Key &key) {
-    // TODO
+    db_->Delete(leveldb::WriteOptions(), key)
     return outcome::success();
   }
 
