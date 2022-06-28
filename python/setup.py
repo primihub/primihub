202ed896a57a7803b38826feff01fe31f1ed7d31
李志威 <lzw9560@gmail.com>
2022-06-28 17:55:34 +0800

    [feat] setup develop/install add compile proto

--- python/setup.py
+++ python/setup.py
@@ -1,5 +1,7 @@
 import glob
 import os
+import shlex
+import subprocess
 import sys
 from distutils.sysconfig import get_python_lib
 from os import path
@@ -29,33 +31,29 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 
-class PostDevelopCommand(develop):
-    """Post-installation for development mode."""
+# class PostDevelopCommand(develop):
+#     """Post-installation for development mode."""
 
-    def run(self):
-        # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
-        develop.run(self)
+#     def run(self):
+#         # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
+#         develop.run(self)
 
 
-class PostInstallCommand(install):
-    """Post-installation for installation mode."""
+# class PostInstallCommand(install):
+#     """Post-installation for installation mode."""
 
-    def run(self):
-        # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
-        install.run(self)
+#     def run(self):
+#         # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
+#         install.run(self)
 
 
 with open('requirements.txt', encoding='utf-8') as reqs:
     install_requires = [l for l in reqs.read().split('\n') if is_pkg(l)]
 
 
-class PostDevelopCommand(develop):
-    """Post-installation for development mode."""
-
-    def run(self):
-        # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
-        develop.run(self)
+def add_primihub_so_site():
         # add ../bazel-bin to PYTHONPATH on LINUX
+        print("ADD ../bazel-bin to PYTHONPATH on LINUX")
         SO_LIB_PATH = path.abspath(path.join(path.dirname(__file__), "../bazel-bin"))  # noqa
         print("* " * 30)
         site_packages_path = get_python_lib()
@@ -69,28 +67,42 @@ class PostDevelopCommand(develop):
             pth.write(SO_LIB_PATH)
 
         print("sys path: ", sys.path)
-        # target = get_python_lib()
-        # for f in glob.glob(SO_LIB):
-        #
-        #     dest = os.path.join(target, os.path.basename(f))
-        #     try:
-        #         os.remove(dest)
-        #     except:
-        #         pass
-        #
-        #     try:
-        #         print("cp {} to {}".format(f, dest))
-        #         copy(f, target)
-        #     except:
-        #         print("Failed to copy file: ", sys.exc_info())
-        # print("* " * 30)
 
+def compile_proto():
+    print("compile proto...")
+    print(os.getcwd())
+    os.chdir("..")
+    print(os.getcwd())
+    cmd = shlex.split("""python -m grpc_tools.protoc \
+                        --python_out=python/primihub/client/ph_grpc \
+                        --grpc_python_out=python/primihub/client/ph_grpc -I. \
+                        src/primihub/protos/common.proto  \
+                        src/primihub/protos/pir.proto \
+                        src/primihub/protos/psi.proto \
+                        src/primihub/protos/service.proto \
+                        src/primihub/protos/worker.proto """)
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    if p.returncode != 0:
+        raise RuntimeError("Error compiling proto file: {0}".format(err)) 
+    os.chdir("python")
+    print(os.getcwd())
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+
+    def run(self):
+        # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
+        add_primihub_so_site()
+        compile_proto()
+        develop.run(self)
 
 class PostInstallCommand(install):
     """Post-installation for installation mode."""
 
     def run(self):
         # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
+        add_primihub_so_site()
+        compile_proto()
         install.run(self)
 
 
