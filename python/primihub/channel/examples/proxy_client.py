2aebf945d72a23404ee810e68766174c09b39acd
zhangxiangyu <zhangxiangyu@primihub.com>
2022-06-28 21:00:01 +0800

    Remove redundant code.

--- python/primihub/channel/examples/proxy_client.py
+++ python/primihub/channel/examples/proxy_client.py
@@ -3,16 +3,6 @@ from primihub.channel.proxy import ClientChannelProxy
 from concurrent.futures import ThreadPoolExecutor
 import time
 
-def server_fn():
-    proxy = ServerChannelProxy("10090")
-    proxy.StartRecvLoop();
-    
-    print(proxy.Get("test_1", 100))
-    print(proxy.Get("test_2", 100))
-    print(proxy.Get("test_3", 100))
-    
-    proxy.StopRecvLoop()
-
 if __name__ == '__main__':
     proxy = ClientChannelProxy("127.0.0.1", "10090")
     proxy.Remote("test_1", "test_1")
