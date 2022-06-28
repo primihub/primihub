147ae8c314ac2a6900c1a487f3a35f618ffdf070
李志威 <lzw9560@gmail.com>
2022-06-28 18:28:01 +0800

    [add] require sphinx

--- python/primihub/channel/examples/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# __init__.py
-# @Author :  ()
-# @Link   : 
-# @Date   : 6/24/2022, 10:20:08 AM
--- python/primihub/client/ph_grpc/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# __init__.py
-# @Author :  ()
-# @Link   : 
-# @Date   : 6/17/2022, 5:46:56 PM
