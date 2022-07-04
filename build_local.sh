ed8367997dc63891fe95316dccfaf643f5fc0b8c
李志威 <lzw9560@gmail.com>
2022-07-04 17:45:56 +0800

    [chore] :construction: fix build script

--- build_local.sh
+++ build_local.sh
@@ -34,18 +34,18 @@ IMAGE_NAME="primihub/primihub_node"
 
 sed -i "10c ARG TARGET_PATH=$BASE_DIR" Dockerfile.local
 
-rm -rf $BASE_DIR/python $BASE_DIR/config
+#rm -rf $BASE_DIR/python $BASE_DIR/config
 rm -f $BASE_DIR/Dockerfile.local
 rm -f $BASE_DIR/.dockerignore
-rm -rf $BASE_DIR/data
+#rm -rf $BASE_DIR/data
 
 cp -r ./data $BASE_DIR/
 cp -r ./python $BASE_DIR/
 cp -r ./config $BASE_DIR/
 cp ./Dockerfile.local $BASE_DIR/
+cp -r ./src $BASE_DIR/
 
 cd $BASE_DIR
 find ./ -name "_objs" > .dockerignore
 
 docker build -t $IMAGE_NAME:$1 . -f Dockerfile.local
-
