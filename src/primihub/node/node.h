3869f67cd240caf7e67d9ac78d0edf25627d814f
hobo0cn <hobo0cn@gmail.com>
2022-07-14 16:28:10 +0800

    [fix] load db from leveldb dump
    LR result dataset wrong data url, and dump when it loaded

--- src/primihub/node/node.h
+++ src/primihub/node/node.h
@@ -87,6 +87,10 @@ class VMNodeImpl final: public VMNode::Service {
         running_set.clear();
         nodelet = std::make_shared<Nodelet>(config_file_path);
     }
+    ~VMNodeImpl() override {
+      std::cout << "VMNodeImpl destructor\n";
+      this->nodelet.reset();
+    }
 
     Status SubmitTask(ServerContext *context,
                       const PushTaskRequest *pushTaskRequest,
