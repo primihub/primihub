095dc0eee5d29987a7a6a011eae1ceb9900dd2db
李志威 <lzw9560@gmail.com>
2022-07-14 10:58:29 +0800

    [fix]

--- python/primihub/client/examples/demo.py
+++ python/primihub/client/examples/demo.py
@@ -16,34 +16,33 @@ limitations under the License.
 
 import primihub as ph
 from primihub.client.client import primihub_cli as cli
+from primihub import context, dataset
 
 # client init
-cli.init(config={"node": "192.168.99.16:50050", "cert": ""})
+# cli.init(config={"node": "192.168.99.16:50050", "cert": ""})
+cli.init(config={"node": "127.0.0.1:8051", "cert": ""})
 
 
 # define a remote method
-# @ph.context.function(protocol="my_protocol", datasets="", next_peer="", role="host")
-@ph.context.function(role='host', protocol='xgboost', datasets=["label_dataset"], next_peer="*:5555")
+@ph.context.function(role='host', protocol='xgboost', datasets=["label_dataset"], next_peer="*:12120")
 def func1(value):
     print("params: ", value)
-    # print(protocol, dataset, role, next_peer)
+    # do something
     return value + 1
 
 
 # define a remote method
-# @ph.context.function(protocol="my_protocol", datasets="", next_peer="", role="guest")
-@ph.context.function(role='guest', protocol='xgboost', datasets=["guest_dataset"], next_peer="localhost:5555")
+@ph.context.function(role='guest', protocol='xgboost', datasets=["guest_dataset"], next_peer="localhost:12120")
 def func2(value):
     print("params: ", value)
-    # print(protocol, dataset, role, next_peer)
+    # do something
     return value + 1
 
 # context
-
 value1 = 1
 value2 = 2
 cli.remote_execute((func1, value1), (func2, value2))
-print(ph.context.Context.output_path)
-print(ph.context.Context.func_params_map)
+print(ph.context.Context)
+print(ph.context.Context.__dict__)
 # ph.context.Context.params_map = {'func1': (1,), 'func2': (1,)}
 # map >>> node context
