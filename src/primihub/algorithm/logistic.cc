14b4d5f2fa85b519614514e5c6d8c59f9b8d2220
keepmoving <zxy267304@gmail.com>
2022-07-15 13:53:00 +0800

    modified logistic.cc 511 line Function 'getDriver's param2

--- src/primihub/algorithm/logistic.cc
+++ src/primihub/algorithm/logistic.cc
@@ -20,6 +20,7 @@
 #include <arrow/result.h>
 #include <glog/logging.h>
 
+#include "src/primihub/node/node.h"
 #include "src/primihub/algorithm/logistic.h"
 #include "src/primihub/data_store/factory.h"
 #include "src/primihub/service/dataset/model.h"
@@ -29,7 +30,7 @@ using namespace Eigen;
 using arrow::Array;
 using arrow::DoubleArray;
 using arrow::Table;
-
+extern primihub::VMNodeImpl * node_service;
 namespace primihub {
 eMatrix<double>
 logistic_main(sf64Matrix<D> &train_data_0_1, sf64Matrix<D> &train_label_0_1,
@@ -508,7 +509,7 @@ int LogisticRegressionExecutor::saveModel(void) {
   std::shared_ptr<arrow::Table> table = arrow::Table::Make(schema, {array});
 
   std::shared_ptr<DataDriver> driver =
-      DataDirverFactory::getDriver("CSV", "test addr");
+      DataDirverFactory::getDriver("CSV", node_service->getNodelet()->getNodeletAddr());
   std::shared_ptr<CSVDriver> csv_driver =
       std::dynamic_pointer_cast<CSVDriver>(driver);
 
