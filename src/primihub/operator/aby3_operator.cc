a042ecf24232bbd5027a94e8dc236087c2748a39
keepmoving <zxy267304@gmail.com>
2022-08-12 15:58:40 +0800

    modify aby3_operator.h and aby3_operator.cc

--- src/primihub/operator/aby3_operator.cc
+++ src/primihub/operator/aby3_operator.cc
@@ -1,63 +1,240 @@
 
 #include "src/primihub/operator/aby3_operator.h"
 
-namespace primihub
-{
-	int MPCOperator::setup(std::string ip, u32 next_port, u32 prev_port)
-	{
-		CommPkg comm = CommPkg();
-		Session ep_next_;
-		Session ep_prev_;
+namespace primihub {
+int MPCOperator::setup(std::string ip, u32 next_port, u32 prev_port) {
+  CommPkg comm = CommPkg();
+  Session ep_next_;
+  Session ep_prev_;
 
-		switch (partyIdx)
-		{
-		case 0:
-			ep_next_.start(ios, ip, next_port, SessionMode::Server, next_name);
-			ep_prev_.start(ios, ip, prev_port, SessionMode::Server, prev_name);
-			break;
-		case 1:
-			ep_next_.start(ios, ip, next_port, SessionMode::Server, next_name);
-			ep_prev_.start(ios, ip, prev_port, SessionMode::Client, prev_name);
-			break;
-		default:
-			ep_next_.start(ios, ip, next_port, SessionMode::Client, next_name);
-			ep_prev_.start(ios, ip, prev_port, SessionMode::Client, prev_name);
-			break;
-		}
-		comm.setNext(ep_next_.addChannel());
-		comm.setPrev(ep_prev_.addChannel());
-		comm.mNext().waitForConnection();
-		comm.mPrev().waitForConnection();
-		comm.mNext().send(partyIdx);
-		comm.mPrev().send(partyIdx);
+  switch (partyIdx) {
+  case 0:
+    ep_next_.start(ios, ip, next_port, SessionMode::Server, next_name);
+    ep_prev_.start(ios, ip, prev_port, SessionMode::Server, prev_name);
+    break;
+  case 1:
+    ep_next_.start(ios, ip, next_port, SessionMode::Server, next_name);
+    ep_prev_.start(ios, ip, prev_port, SessionMode::Client, prev_name);
+    break;
+  default:
+    ep_next_.start(ios, ip, next_port, SessionMode::Client, next_name);
+    ep_prev_.start(ios, ip, prev_port, SessionMode::Client, prev_name);
+    break;
+  }
+  comm.setNext(ep_next_.addChannel());
+  comm.setPrev(ep_prev_.addChannel());
+  comm.mNext().waitForConnection();
+  comm.mPrev().waitForConnection();
+  comm.mNext().send(partyIdx);
+  comm.mPrev().send(partyIdx);
 
-		u64 prev_party = 0;
-		u64 next_party = 0;
-		comm.mNext().recv(next_party);
-		comm.mPrev().recv(prev_party);
-		if (next_party != (partyIdx + 1) % 3)
-		{
-			LOG(ERROR) << "Party " << partyIdx << ", expect next party id "
-					   << (partyIdx + 1) % 3 << ", but give " << next_party << ".";
-			return -3;
-		}
+  u64 prev_party = 0;
+  u64 next_party = 0;
+  comm.mNext().recv(next_party);
+  comm.mPrev().recv(prev_party);
+  if (next_party != (partyIdx + 1) % 3) {
+    LOG(ERROR) << "Party " << partyIdx << ", expect next party id "
+               << (partyIdx + 1) % 3 << ", but give " << next_party << ".";
+    return -3;
+  }
 
-		if (prev_party != (partyIdx + 2) % 3)
-		{
-			LOG(ERROR) << "Party " << partyIdx << ", expect prev party id "
-					   << (partyIdx + 2) % 3 << ", but give " << prev_party << ".";
-			return -3;
-		}
+  if (prev_party != (partyIdx + 2) % 3) {
+    LOG(ERROR) << "Party " << partyIdx << ", expect prev party id "
+               << (partyIdx + 2) % 3 << ", but give " << prev_party << ".";
+    return -3;
+  }
 
-		// Establishes some shared randomness needed for the later protocols
-		enc.init(partyIdx, comm, sysRandomSeed());
+  // Establishes some shared randomness needed for the later protocols
+  enc.init(partyIdx, comm, sysRandomSeed());
 
-		// Establishes some shared randomness needed for the later protocols
-		eval.init(partyIdx, comm, sysRandomSeed());
+  // Establishes some shared randomness needed for the later protocols
+  eval.init(partyIdx, comm, sysRandomSeed());
 
-		// Copies the Channels and will use them for later protcols.
-		auto commPtr = std::make_shared<CommPkg>(comm.mPrev(), comm.mNext());
-		runtime.init(partyIdx, commPtr);
-		return 1;
-	}
+  // Copies the Channels and will use them for later protcols.
+  auto commPtr = std::make_shared<CommPkg>(comm.mPrev(), comm.mNext());
+  runtime.init(partyIdx, commPtr);
+  return 1;
+}
+template <Decimal D>
+sf64Matrix<D> MPCOperator::createShares(const eMatrix<double> &vals,
+                                        sf64Matrix<D> &sharedMatrix) {
+  f64Matrix<D> fixedMatrix(vals.rows(), vals.cols());
+  for (u64 i = 0; i < vals.size(); ++i)
+    fixedMatrix(i) = vals(i);
+  enc.localFixedMatrix(runtime, fixedMatrix, sharedMatrix).get();
+  return sharedMatrix;
+}
+
+template <Decimal D>
+sf64Matrix<D> MPCOperator::createShares(sf64Matrix<D> &sharedMatrix) {
+  enc.remoteFixedMatrix(runtime, sharedMatrix).get();
+  return sharedMatrix;
+}
+
+si64Matrix MPCOperator::createShares(const i64Matrix &vals,
+                                     si64Matrix &sharedMatrix) {
+  enc.localIntMatrix(runtime, vals, sharedMatrix).get();
+  return sharedMatrix;
+}
+
+si64Matrix MPCOperator::createShares(si64Matrix &sharedMatrix) {
+  enc.remoteIntMatrix(runtime, sharedMatrix).get();
+  return sharedMatrix;
+}
+
+template <Decimal D>
+sf64<D> MPCOperator::createShares(double vals, sf64<D> &sharedFixedInt) {
+  enc.localFixed<D>(runtime, vals, sharedFixedInt).get();
+  return sharedFixedInt;
+}
+
+template <Decimal D>
+sf64<D> MPCOperator::createShares(sf64<D> &sharedFixedInt) {
+  enc.remoteFixed<D>(runtime, sharedFixedInt).get();
+  return sharedFixedInt;
+}
+
+template <Decimal D>
+eMatrix<double> MPCOperator::revealAll(const sf64Matrix<D> &vals) {
+  f64Matrix<D> temp(vals.rows(), vals.cols());
+  enc.revealAll(runtime, vals, temp).get();
+
+  eMatrix<double> ret(vals.rows(), vals.cols());
+  for (u64 i = 0; i < ret.size(); ++i)
+    ret(i) = static_cast<double>(temp(i));
+  return ret;
+}
+
+i64Matrix MPCOperator::revealAll(const si64Matrix &vals) {
+  i64Matrix ret(vals.rows(), vals.cols());
+  enc.revealAll(runtime, vals, ret).get();
+  return ret;
+}
+
+template <Decimal D> double MPCOperator::revealAll(const sf64<D> &vals) {
+  f64<D> ret;
+  enc.revealAll(runtime, vals, ret).get();
+  return static_cast<double>(ret);
+}
+
+template <Decimal D>
+eMatrix<double> MPCOperator::reveal(const sf64Matrix<D> &vals) {
+  f64Matrix<D> temp(vals.rows(), vals.cols());
+  enc.reveal(runtime, vals, temp).get();
+  eMatrix<double> ret(vals.rows(), vals.cols());
+  for (u64 i = 0; i < ret.size(); ++i)
+    ret(i) = static_cast<double>(temp(i));
+  return ret;
+}
+
+template <Decimal D>
+void MPCOperator::reveal(const sf64Matrix<D> &vals, u64 Idx) {
+  enc.reveal(runtime, Idx, vals).get();
+}
+
+i64Matrix MPCOperator::reveal(const si64Matrix &vals) {
+  i64Matrix ret(vals.rows(), vals.cols());
+  enc.reveal(runtime, vals, ret).get();
+  return ret;
+}
+
+void MPCOperator::reveal(const si64Matrix &vals, u64 Idx) {
+  enc.reveal(runtime, Idx, vals).get();
+}
+
+template <Decimal D> double MPCOperator::reveal(const sf64<D> &vals, u64 Idx) {
+  if (Idx == partyIdx) {
+    f64<D> ret;
+    enc.reveal(runtime, vals, ret).get();
+    return static_cast<double>(ret);
+  } else {
+    enc.reveal(runtime, Idx, vals).get();
+  }
+}
+
+template <Decimal D>
+sf64<D> MPCOperator::MPC_Add(std::vector<sf64<D>> sharedFixedInt,
+                             sf64<D> &sum) {
+  sum = sharedFixedInt[0];
+  for (u64 i = 1; i < sharedFixedInt.size(); i++) {
+    sum = sum + sharedFixedInt[i];
+  }
+  return sum;
+}
+
+template <Decimal D>
+sf64Matrix<D> MPCOperator::MPC_Add(std::vector<sf64Matrix<D>> sharedFixedInt,
+                                   sf64Matrix<D> &sum) {
+  sum = sharedFixedInt[0];
+  for (u64 i = 1; i < sharedFixedInt.size(); i++) {
+    sum = sum + sharedFixedInt[i];
+  }
+  return sum;
+}
+
+si64Matrix MPCOperator::MPC_Add(std::vector<si64Matrix> sharedInt,
+                                si64Matrix &sum) {
+  sum = sharedInt[0];
+  for (u64 i = 1; i < sharedInt.size(); i++) {
+    sum = sum + sharedInt[i];
+  }
+  return sum;
+}
+
+template <Decimal D>
+sf64<D> MPCOperator::MPC_Sub(sf64<D> minuend, std::vector<sf64<D>> subtrahends,
+                             sf64<D> &difference) {
+  difference = minuend;
+  for (u64 i = 0; i < subtrahends.size(); i++) {
+    difference = difference - subtrahends[i];
+  }
+  return difference;
+}
+
+template <Decimal D>
+sf64Matrix<D> MPCOperator::MPC_Sub(sf64Matrix<D> minuend,
+                                   std::vector<sf64Matrix<D>> subtrahends,
+                                   sf64Matrix<D> &difference) {
+  difference = minuend;
+  for (u64 i = 0; i < subtrahends.size(); i++) {
+    difference = difference - subtrahends[i];
+  }
+  return difference;
+}
+
+si64Matrix MPCOperator::MPC_Sub(si64Matrix minuend,
+                                std::vector<si64Matrix> subtrahends,
+                                si64Matrix &difference) {
+  difference = minuend;
+  for (u64 i = 0; i < subtrahends.size(); i++) {
+    difference = difference - subtrahends[i];
+  }
+  return difference;
+}
+
+template <Decimal D>
+sf64<D> MPCOperator::MPC_Mul(std::vector<sf64<D>> sharedFixedInt,
+                             sf64<D> &prod) {
+  prod = sharedFixedInt[0];
+  for (u64 i = 1; i < sharedFixedInt.size(); ++i)
+    eval.asyncMul(runtime, prod, sharedFixedInt[i], prod).get();
+  return prod;
+}
+
+template <Decimal D>
+sf64Matrix<D> MPCOperator::MPC_Mul(std::vector<sf64Matrix<D>> sharedFixedInt,
+                                   sf64Matrix<D> &prod) {
+  prod = sharedFixedInt[0];
+  for (u64 i = 1; i < sharedFixedInt.size(); ++i)
+    eval.asyncMul(runtime, prod, sharedFixedInt[i], prod).get();
+  return prod;
+}
+
+si64Matrix MPCOperator::MPC_Mul(std::vector<si64Matrix> sharedInt,
+                                si64Matrix &prod) {
+  prod = sharedInt[0];
+  for (u64 i = 1; i < sharedInt.size(); ++i)
+    eval.asyncMul(runtime, prod, sharedInt[i], prod).get();
+  return prod;
+}
 } // namespace primihub
--- test/primihub/algorithm/aby3_add_test.cc
+++ test/primihub/algorithm/aby3_add_test.cc
@@ -4,136 +4,118 @@
 // #include "src/primihub/algorithm/logistic.h"
 // #include "src/primihub/service/dataset/localkv/storage_default.h"
 
-#include "src/primihub/util/network/socket/ioservice.h"
-#include "src/primihub/protocol/aby3/runtime.h"
+#include "src/primihub/operator/aby3_operator.h"
 #include "src/primihub/protocol/aby3/encryptor.h"
 #include "src/primihub/protocol/aby3/evaluator/evaluator.h"
-#include "src/primihub/util/network/socket/session.h"
+#include "src/primihub/protocol/aby3/runtime.h"
 #include "src/primihub/util/network/socket/commpkg.h"
-#include "src/primihub/operator/aby3_operator.h"
+#include "src/primihub/util/network/socket/ioservice.h"
+#include "src/primihub/util/network/socket/session.h"
 using namespace primihub;
 
-TEST(add_operator, aby3_3pc_test)
-{
-	pid_t pid = fork();
-	if (pid != 0)
-	{
-		// Child process as party 0.
-		// integerOperations(0);
-		// fixedPointOperations(0);
-		MPCOperator mpc(0, "01", "02");
-		mpc.setup("127.0.0.1", (u32)1313, (u32)1414);
-		u64 rows = 2,
-			cols = 2;
-		// input data
-		eMatrix<i64> plainMatrix(rows, cols);
-		for (u64 i = 0; i < rows; ++i)
-			for (u64 j = 0; j < cols; ++j)
-				plainMatrix(i, j) = i + j;
+TEST(add_operator, aby3_3pc_test) {
+  pid_t pid = fork();
+  if (pid != 0) {
+    // Child process as party 0.
+    // integerOperations(0);
+    // fixedPointOperations(0);
+    MPCOperator mpc(0, "01", "02");
+    mpc.setup("127.0.0.1", (u32)1313, (u32)1414);
+    u64 rows = 2, cols = 2;
+    // input data
+    eMatrix<i64> plainMatrix(rows, cols);
+    for (u64 i = 0; i < rows; ++i)
+      for (u64 j = 0; j < cols; ++j)
+        plainMatrix(i, j) = i + j;
 
-		std::vector<si64Matrix> sharedMatrix;
-		sharedMatrix.emplace_back(si64Matrix(rows, cols));
-		sharedMatrix.emplace_back(si64Matrix(rows, cols));
-		sharedMatrix.emplace_back(si64Matrix(rows, cols));
-		// construct shares
-		for (u64 i = 0; i < 3; i++)
-		{
-			if (i == mpc.partyIdx)
-			{
-				mpc.createShares(plainMatrix, sharedMatrix[i]);
-			}
-			else
-			{
-				mpc.createShares(sharedMatrix[i]);
-			}
-		}
-		// Add
-		//  si64Matrix MPC_Add(std::vector<si64Matrix> sharedInt, si64Matrix &sum)
-		si64Matrix sum;
-		mpc.MPC_Add(sharedMatrix, sum);
-		LOG(INFO) << "starting reveal";
+    std::vector<si64Matrix> sharedMatrix;
+    sharedMatrix.emplace_back(si64Matrix(rows, cols));
+    sharedMatrix.emplace_back(si64Matrix(rows, cols));
+    sharedMatrix.emplace_back(si64Matrix(rows, cols));
+    // construct shares
+    for (u64 i = 0; i < 3; i++) {
+      if (i == mpc.partyIdx) {
+        mpc.createShares(plainMatrix, sharedMatrix[i]);
+      } else {
+        mpc.createShares(sharedMatrix[i]);
+      }
+    }
+    // Add
+    //  si64Matrix MPC_Add(std::vector<si64Matrix> sharedInt, si64Matrix &sum)
+    si64Matrix sum;
+    mpc.MPC_Add(sharedMatrix, sum);
+    LOG(INFO) << "starting reveal";
 
-		i64Matrix sumVal = mpc.reveal(sum);
-		LOG(INFO) << "Sum:" << sumVal;
-		return;
-	}
+    i64Matrix sumVal = mpc.reveal(sum);
+    LOG(INFO) << "Sum:" << sumVal;
+    return;
+  }
 
-	pid = fork();
-	if (pid != 0)
-	{
-		// Child process as party 1.
-		sleep(1);
-		// integerOperations(1);
-		// fixedPointOperations(1);
-		MPCOperator mpc(1, "12", "01");
-		mpc.setup("127.0.0.1", (u32)1515, (u32)1313);
-		u64 rows = 2,
-			cols = 2;
-		// input data
-		eMatrix<i64> plainMatrix(rows, cols);
-		for (u64 i = 0; i < rows; ++i)
-			for (u64 j = 0; j < cols; ++j)
-				plainMatrix(i, j) = i + j ;
-		std::vector<si64Matrix> sharedMatrix;
-		sharedMatrix.emplace_back(si64Matrix(rows, cols));
-		sharedMatrix.emplace_back(si64Matrix(rows, cols));
-		sharedMatrix.emplace_back(si64Matrix(rows, cols));
-		// construct shares
-		for (u64 i = 0; i < 3; i++)
-		{
-			if (i == mpc.partyIdx)
-			{
-				mpc.createShares(plainMatrix, sharedMatrix[i]);
-			}
-			else
-			{
-				mpc.createShares(sharedMatrix[i]);
-			}
-		}
-		// Add
-		//  si64Matrix MPC_Add(std::vector<si64Matrix> sharedInt, si64Matrix &sum)
-		si64Matrix sum;
-		mpc.MPC_Add(sharedMatrix, sum);
-		mpc.reveal(sum, 0);
+  pid = fork();
+  if (pid != 0) {
+    // Child process as party 1.
+    sleep(1);
+    // integerOperations(1);
+    // fixedPointOperations(1);
+    MPCOperator mpc(1, "12", "01");
+    mpc.setup("127.0.0.1", (u32)1515, (u32)1313);
+    u64 rows = 2, cols = 2;
+    // input data
+    eMatrix<i64> plainMatrix(rows, cols);
+    for (u64 i = 0; i < rows; ++i)
+      for (u64 j = 0; j < cols; ++j)
+        plainMatrix(i, j) = i + j;
+    std::vector<si64Matrix> sharedMatrix;
+    sharedMatrix.emplace_back(si64Matrix(rows, cols));
+    sharedMatrix.emplace_back(si64Matrix(rows, cols));
+    sharedMatrix.emplace_back(si64Matrix(rows, cols));
+    // construct shares
+    for (u64 i = 0; i < 3; i++) {
+      if (i == mpc.partyIdx) {
+        mpc.createShares(plainMatrix, sharedMatrix[i]);
+      } else {
+        mpc.createShares(sharedMatrix[i]);
+      }
+    }
+    // Add
+    //  si64Matrix MPC_Add(std::vector<si64Matrix> sharedInt, si64Matrix &sum)
+    si64Matrix sum;
+    mpc.MPC_Add(sharedMatrix, sum);
+    mpc.reveal(sum, 0);
 
-		return;
-	}
+    return;
+  }
 
-	// Parent process as party 2.
-	sleep(3);
+  // Parent process as party 2.
+  sleep(3);
 
-	// integerOperations(2);
-	// fixedPointOperations(2);
-	MPCOperator mpc(2, "02", "12");
-	mpc.setup("127.0.0.1", (u32)1414, (u32)1515);
-	u64 rows = 2,
-		cols = 2;
-	// input data
-	eMatrix<i64> plainMatrix(rows, cols);
-	for (u64 i = 0; i < rows; ++i)
-		for (u64 j = 0; j < cols; ++j)
-			plainMatrix(i, j) = i + j + 1;
+  // integerOperations(2);
+  // fixedPointOperations(2);
+  MPCOperator mpc(2, "02", "12");
+  mpc.setup("127.0.0.1", (u32)1414, (u32)1515);
+  u64 rows = 2, cols = 2;
+  // input data
+  eMatrix<i64> plainMatrix(rows, cols);
+  for (u64 i = 0; i < rows; ++i)
+    for (u64 j = 0; j < cols; ++j)
+      plainMatrix(i, j) = i + j + 1;
 
-	std::vector<si64Matrix> sharedMatrix;
-	sharedMatrix.emplace_back(si64Matrix(rows, cols));
-	sharedMatrix.emplace_back(si64Matrix(rows, cols));
-	sharedMatrix.emplace_back(si64Matrix(rows, cols));
-	// construct shares
-	for (u64 i = 0; i < 3; i++)
-	{
-		if (i == mpc.partyIdx)
-		{
-			mpc.createShares(plainMatrix, sharedMatrix[i]);
-		}
-		else
-		{
-			mpc.createShares(sharedMatrix[i]);
-		}
-	}
-	// Add
-	//  si64Matrix MPC_Add(std::vector<si64Matrix> sharedInt, si64Matrix &sum)
-	si64Matrix sum;
-	mpc.MPC_Add(sharedMatrix, sum);
-	mpc.reveal(sum, 0);
-	return;
+  std::vector<si64Matrix> sharedMatrix;
+  sharedMatrix.emplace_back(si64Matrix(rows, cols));
+  sharedMatrix.emplace_back(si64Matrix(rows, cols));
+  sharedMatrix.emplace_back(si64Matrix(rows, cols));
+  // construct shares
+  for (u64 i = 0; i < 3; i++) {
+    if (i == mpc.partyIdx) {
+      mpc.createShares(plainMatrix, sharedMatrix[i]);
+    } else {
+      mpc.createShares(sharedMatrix[i]);
+    }
+  }
+  // Add
+  //  si64Matrix MPC_Add(std::vector<si64Matrix> sharedInt, si64Matrix &sum)
+  si64Matrix sum;
+  mpc.MPC_Add(sharedMatrix, sum);
+  mpc.reveal(sum, 0);
+  return;
 }
