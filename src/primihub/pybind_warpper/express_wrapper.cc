1a57c83e7b3e964a6afc70e6157566ed4cb4ca37
ZhangXiangyu <zxy267304@gmail.com>
2022-09-16 16:17:21 +0800

    Working on python binding for local express executor.

--- src/primihub/pybind_warpper/express_wrapper.cc
+++ src/primihub/pybind_warpper/express_wrapper.cc
@@ -97,6 +97,15 @@ void PyMPCExpressExecutor::importFP64ColumnValues(std::string &name,
   return;
 }
 
+void PyMPCExpressExecutor::importColumnValues(std::string &name,
+                                              py::list &val_list) {
+  PyObject *obj = val_list[0].ptr();
+  if (PyFloat_Check(obj))
+    importFP64ColumnValues(name, val_list);
+  else
+    importI64ColumnValues(name, val_list);
+}
+
 void PyMPCExpressExecutor::runMPCEvaluate(uint32_t party_id,
                                           const std::string &ip,
                                           uint16_t next_port,
@@ -156,9 +165,18 @@ void PyLocalExpressExecutor::importI64ColumnValues(std::string &owner,
                                                    py::list &val_list) {
   std::vector<int64_t> val_vec;
   for (auto v : val_list)
-    val_vec.push_back(v.cast<int64_t<());
+    val_vec.push_back(v.cast < int64_t < ());
 
   i64_val_map_.insert(std::make_pair(owner, val_vec));
+}
+
+void PyLocalExpressExecutor::importColumnValues(std::string &owner,
+                                                py::list &val_list) {
+  PyObject *obj = val_list[0].ptr();
+  if (PyFloat_Check(obj))
+    importFP64ColumnValues(owner, val_list);
+  else
+    importI64ColumnValues(owner, val_list);
 }
 
 void PyLocalExpressExecutor::finishImport(void) {
@@ -166,6 +184,32 @@ void PyLocalExpressExecutor::finishImport(void) {
     LocalExpressExecutor::init(fp64_val_map_);
   else
     LocalExpressExecutor::init(i64_val_map_);
+}
+
+py::object PyLocalExpressExecutor::runLocalEvaluate(void) {
+  if (LocalExpressExecutor::runLocalEvaluate()) {
+    throw std::runtime_error("run express failed.");
+    return py::cast<py::none>(Py_None);
+  }
+
+  if (LocalExpressExecutor::isFP64RunMode()) {
+    std::vector<double> result;
+    LocalExpressExecutor::getFinalVal(result);
+
+    py::list val_list;
+    for (auto v : result)
+      val_list.append(v);
+    return val_list;
+
+  } else {
+    std::vector<int64_t> result;
+    LocalExpressExecutor::getFinalVal(result);
+
+    py::list val_list;
+    for (auto v : result)
+      val_list.append(v);
+    return val_list;
+  }
 }
 
 }; // namespace primihub
--- src/primihub/pybind_warpper/express_wrapper.h
+++ src/primihub/pybind_warpper/express_wrapper.h
@@ -9,47 +9,52 @@
 namespace py = pybind11;
 
 namespace primihub {
-// This class is a wrapper of C++'s express executor with MPC protocol, 
+// This class is a wrapper of C++'s express executor with MPC protocol,
 // and will be used by python interpreter.
 class PyMPCExpressExecutor : public MPCExpressExecutor {
 public:
   PyMPCExpressExecutor(uint32_t party_id);
-  
-  // These methods are called by python interpreter, 
+
+  // These methods are called by python interpreter,
   // ensure call them with below order.
- 
+
   // 1. Import column owner and dtype.
   void importColumnConfig(py::dict &col_owner, py::dict &col_dtype);
-  
+
   // 2. Import express.
   void importExpress(const std::string &expr);
-  
+
   // 3. Import column values.
-  void importFP64ColumnValues(std::string &col_name, py::list &val_list);
-  void importI64ColumnValues(std::string &col_name, py::list &val_list);
-  
+  void importColumnValues(std::string &col_name, py::list &val_list);
+
   // 4. Evaluate express with MPC protocol.
   void runMPCEvaluate(uint32_t party_id, const std::string &ip,
                       uint16_t next_port, uint16_t prev_port);
-  
-  // 5. Reveal MPC result to parties. 
+
+  // 5. Reveal MPC result to parties.
   py::object revealMPCResult(py::list &party_list);
 
 private:
+  inline void importFP64ColumnValues(std::string &col_name, py::list &val_list);
+  inline void importI64ColumnValues(std::string &col_name, py::list &val_list);
+
   uint32_t party_id_;
 };
 
+// Warning: this class is only used for test, DON'T USE IT IN ANY APPLICATION.
 class PyLocalExpressExecutor : public LocalExpressExecutor {
 public:
   PyLocalExpressExecutor(PyMPCExpressExecutor *mpc_exec);
-  void importFP64ColumnValues(std::string &owner, py::list &val_list);
-  void importI64ColumnValues(std::string &owner, py::list &val_list);
+  void importColumnValues(std::string &name, py::list &val_list);
   void finishImport(void);
-  py::list runLocalEvaluate(void);
+  py::object runLocalEvaluate(void);
 
 private:
-  std::map<std::string, std::vector<double> fp64_val_map_;
-  std::map<std::string, std::vector<int64_t> i64_val_map_;
+  inline void importFP64ColumnValues(std::string &owner, py::list &val_list);
+  inline void importI64ColumnValues(std::string &owner, py::list &val_list);
+
+  std::map<std::string, std::vector<double>> fp64_val_map_;
+  std::map<std::string, std::vector<int64_t>> i64_val_map_;
 };
 
 } // namespace primihub
