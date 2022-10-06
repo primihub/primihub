d397192dfa99de85500f8d578e618c4d535083bf
Victory <lzw9560@gmail.com>
2022-10-06 20:44:44 +0800

    update

--- python/primihub/client/__init__.py
+++ python/primihub/client/__init__.py
@@ -2,3 +2,6 @@ from primihub.client.client import PrimihubClient
 
 # alias
 primihub_cli = PrimihubClient()
+
+# from primihub.client.ray_client import PrimihubClient
+# primihub_cli = PrimihubClient()
