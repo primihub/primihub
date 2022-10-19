72091d0b198af27fed5b31412df7b979b4e1b315
zxy <zxy267304@gmail.com>
2022-10-19 19:30:05 +0800

    Add output for empty position in csv.

--- python/primihub/dataset/abnormal.py
+++ python/primihub/dataset/abnormal.py
@@ -5,30 +5,32 @@ import pandas as pd
 from loguru import logger
 
 
-def handle_mixed_row(row):
-    row_sum = 0
-    row_count = 0
+def handle_mixed_column(col):
+    col_sum = 0
+    col_count = 0
     index_list = []
-    for index, val in row.items():
+    for index, val in col.items():
         if val == "NA":
+            logger.info("Column {} index {} has empty value.".format(
+                col.name, index))
             continue
 
         try:
             tmp = float(val)
-            row_sum = row_sum + tmp
-            row_count = row_count + 1
+            col_sum = col_sum + tmp
+            col_count = col_count + 1
         except:
             index_list.append(index)
 
-    if len(index_list) != 0 and row_sum != 0:
-        row_avg = row_sum / row_count
+    if len(index_list) != 0 and col_sum != 0:
+        col_avg = col_sum / col_count
         for index in index_list:
             logger.info("Replace {} with {}, column {}, index {}.".format(
-                row[index], row_avg, row.name, index))
-            row[index] = str(row_avg)
+                col[index], col_avg, col.name, index))
+            col[index] = str(col_avg)
     else:
         logger.info(
-            "All content of column {} is string, do nothing.".format(row.name))
+            "All content of column {} is string, do nothing.".format(col.name))
 
 
 @ph.context.function(role="host", protocol="None", datasets=["train_party_0"], port="-1")
@@ -54,8 +56,8 @@ def run_abnormal_process():
 
         for col_name, type in col_info.items():
             if type == 1 or type == 2 or type == 3:
-                row = df[col_name]
-                if row.dtype == object:
-                    handle_mixed_row(row)
+                col = df[col_name]
+                if col.dtype == object:
+                    handle_mixed_column(col)
 
         df.to_csv("/tmp/output.csv")
