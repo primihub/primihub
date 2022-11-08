00ba6070430c4af3b1f9bf181de4558426223db9
xusong <xu15701157125@163.com>
2022-11-08 13:25:44 +0800

    feat(song.xu):add hetero xgb

--- hetero_xgb.py
+++ hetero_xgb.py
@@ -552,9 +552,6 @@ class XGB_GUEST_EN:
                 self.lookup_table[self.guest_record] = [best_var, best_cut]
                 print("guest look_up table:", self.lookup_table)
 
-                # self.guest_record += 1
-                self.guest_record += 1
-
                 X_guest_left = X_guest.loc[id_left]
                 print("======X_guest_left======", X_guest_left.index)
                 X_guest_right = X_guest.loc[id_right]
@@ -562,17 +559,22 @@ class XGB_GUEST_EN:
                 encrypted_ghs_left = encrypted_ghs.loc[id_left]
                 encrypted_ghs_right = encrypted_ghs.loc[id_right]
 
+                # self.guest_record += 1
+                self.guest_record += 1
+
             else:
                 ids_w = self.proxy_server.Get(str(self.host_record) + '_ids_w')
+                print("guest record: ", self.host_record)
                 role = 'host'
                 record = self.host_record
                 id_left = ids_w['id_left']
                 id_right = ids_w['id_right']
                 w_left = ids_w['w_left']
                 w_right = ids_w['w_right']
-                self.host_record += 1
+                # self.host_record += 1
 
-                print("===train guest==", X_guest.index, ids_w)
+                print("===train guest==", X_guest.index, ids_w,
+                      self.host_record)
                 # tree_structure = {(role, record): {}}
 
                 X_guest_left = X_guest.loc[id_left]
@@ -581,11 +583,12 @@ class XGB_GUEST_EN:
 
                 encrypted_ghs_left = encrypted_ghs.loc[id_left]
                 encrypted_ghs_right = encrypted_ghs.loc[id_right]
+                self.host_record += 1
 
-            self.guest_tree_construct(copy.deepcopy(X_guest_left),
-                                      encrypted_ghs_left, current_depth + 1)
-            self.guest_tree_construct(copy.deepcopy(X_guest_right),
-                                      encrypted_ghs_right, current_depth + 1)
+            self.guest_tree_construct(X_guest_left, encrypted_ghs_left,
+                                      current_depth + 1)
+            self.guest_tree_construct(X_guest_right, encrypted_ghs_right,
+                                      current_depth + 1)
 
             # tree_structure[(role,
             #                 record)][('left',
@@ -963,6 +966,8 @@ class XGB_HOST_EN:
                     'w_left': w_left,
                     'w_right': w_right
                 }
+                print("===train host==", X_host.index, ids_w, self.host_record)
+
                 self.proxy_client_guest.Remote(ids_w,
                                                str(self.host_record) + '_ids_w')
 
@@ -970,7 +975,6 @@ class XGB_HOST_EN:
                     host_best['best_var'], host_best['best_cut']
                 ]
                 # print("self.lookup_table", self.lookup_table)
-                print("===train host==", X_host.index, ids_w)
 
                 self.host_record += 1
 
@@ -992,15 +996,15 @@ class XGB_HOST_EN:
 
             tree_structure[(role, record)][('left',
                                             w_left)] = self.host_tree_construct(
-                                                copy.deepcopy(X_host_left), f_t,
+                                                X_host_left, f_t,
                                                 current_depth + 1,
                                                 plain_gh_left)
 
             tree_structure[(role,
                             record)][('right',
                                       w_right)] = self.host_tree_construct(
-                                          copy.deepcopy(X_host_right), f_t,
-                                          current_depth + 1, plain_gh_right)
+                                          X_host_right, f_t, current_depth + 1,
+                                          plain_gh_right)
 
             return tree_structure
 
