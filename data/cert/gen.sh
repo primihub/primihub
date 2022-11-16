175911850e38720f4e7d65ad16d6111490ea881d
zxy <zxy267304@gmail.com>
2022-11-16 21:07:27 +0800

    Regenerate tls cert.

--- data/cert/gen.sh
+++ data/cert/gen.sh
@@ -4,10 +4,10 @@
 openssl genrsa -out ca.key 2048
 # 生成.csr 证书签名请求文件
 openssl req -new -key ca.key -out ca.csr \
-  -subj "/C=GB/L=China/O=primihub/CN=www.primihub.ca.com"
+  -subj "/C=GB/L=China/O=primihub/CN=primihubnode"
 # 自签名生成.crt/.pem 证书文件
 openssl req -new -x509 -days 3650 -key ca.key -out ca.crt \
-   -subj "/C=GB/L=China/O=primihub/CN=www.primihub.ca.com"
+   -subj "/C=GB/L=China/O=primihub/CN=primihubnode"
 
 
 ############server side certificate #################3
@@ -16,7 +16,7 @@ openssl genrsa -out server.key 2048
 
 # 生成.csr 证书签名请求文件
 openssl req -new -key server.key -out server.csr \
-  -subj "/C=GB/L=China/O=primihub/CN=www.primihub.server.com"
+  -subj "/C=GB/L=China/O=primihub/CN=primihubnode"
 
 # 签名生成.crt/.pem 证书文件
 openssl x509 -req -days 3650 \
@@ -30,7 +30,7 @@ openssl genrsa -out client.key 2048
 
 # 生成.csr 证书签名请求文件
 openssl req -new -key client.key -out client.csr \
-  -subj "/C=GB/L=China/O=primihub/CN=www.primihub.client.com"
+  -subj "/C=GB/L=China/O=primihub/CN=primihubnode_client"
 
 # 签名生成.crt/.pem 证书文件
 openssl x509 -req -days 3650 \
