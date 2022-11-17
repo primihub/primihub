03d13e5e6295b708a96840c709ce5a10dfee8023
zxy <zxy267304@gmail.com>
2022-11-17 13:06:45 +0800

    Debug abnormal value process in sqlite dataset.

--- python/primihub/dataset/abnormal.py
+++ python/primihub/dataset/abnormal.py
@@ -132,8 +132,7 @@ def replace_illegal_string(col_val, col_name, col_type):
 #   value 5: boolean.
 
 
-def handle_abnormal_value_for_sqlite(path_or_info, col_info):
-    db_info = json.loads(path_or_info)
+def handle_abnormal_value_for_sqlite(db_info, col_info):
     db_table_name = db_info["dbTableName"]
 
     conn = sqlite3.connect(db_info["db_path"])
@@ -217,25 +216,19 @@ def run_abnormal_process(params_map, dataset_map):
 
     # Get dataset path or dataset URL.
     dataset_name = params_map["local_dataset"]
-    path_or_info = dataset_map[dataset_name]
-
-    use_db = True
-    try:
-        db_conn_info = json.loads(path_or_info)
+    csv_or_sqlite = dataset_map[dataset_name]
+    
+    # If the dataset is a sqlite database file then the 'csv_or_sqlite' looks 
+    # like "sqlite#/data/upload/1/2022111613/806f3a7b-834e-43f9-8598-69a9f781
+    # d46d.db3#data_mpc_0#", so it should be a sqlite database file when the 
+    # string start with 'sqlite'.
+    use_db = False 
+    if csv_or_sqlite.startswith("sqlite"):
         use_db = True
-    except Exception as e:
+        logger.info("The string '{}' is a sqlite dataset file addr.".format(csv_or_sqlite))
+    else:
         use_db = False
-
-    # TODO: Remove it, add only for test.
-    use_db = True
-    db_info = {}
-    db_info["db_path"] = "/tmp/test.db"
-    db_info["dbTableName"] = "test_table"
-    # db_info["dbName"] = "primihub"
-    # db_info["dbUsername"] = "root"
-    # db_info["dbPassword"] = "123456"
-    path_or_info = json.dumps(db_info)
-    filename = "/tmp/test"
+        logger.info("The string '{}' is a file path.".format(csv_or_sqlite))
 
     col_info_str = params_map["ColumnInfo"]
     all_col_info = json.loads(col_info_str)
@@ -243,8 +236,12 @@ def run_abnormal_process(params_map, dataset_map):
     new_dataset_id = all_col_info[dataset_name]["newDataSetId"]
 
     if use_db is True:
-        df = handle_abnormal_value_for_sqlite(path_or_info, col_dtype)
-        save_path = filename + "_abnormal.csv"
+        db_info = {}
+        substr_arr = csv_or_sqlite.split("#")
+        db_info["db_path"] = substr_arr[1]
+        db_info["dbTableName"] = substr_arr[2]
+        df = handle_abnormal_value_for_sqlite(db_info, col_dtype)
+        save_path = substr_arr[2].split(".db")[0] + "_abnormal.csv"
     else:
         filename, _ = path_or_info.split(".csv")
         save_path = filename + "_abnormal.csv"
