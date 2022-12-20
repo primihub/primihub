c9ad113c3fee5c5aee655b6950544a0fbb88cd79
xusong <xu15701157125@163.com>
2022-12-20 16:21:54 +0800

    feat(song.xu):reset limit size

--- python/primihub/examples/hetero_xgb.py
+++ python/primihub/examples/hetero_xgb.py
@@ -233,7 +233,7 @@ class MyPandasBlockAccessor(PandasBlockAccessor):
             encrypted: bool,
             pub_key: None,
             add_actors,
-            limit=50) -> Optional[U]:
+            limit=30) -> Optional[U]:
         pd = lazy_import_pandas()
         if on is not None and not isinstance(on, str):
             raise ValueError(
