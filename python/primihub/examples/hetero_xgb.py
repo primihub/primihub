8030e2a1051acdd1f9c8207df604cda8433f7989
xusong <xu15701157125@163.com>
2022-12-21 18:45:00 +0800

    feat(song.xu): test epsilon data

--- python/primihub/examples/hetero_xgb.py
+++ python/primihub/examples/hetero_xgb.py
@@ -1798,12 +1798,12 @@ def xgb_host_logic(cry_pri="paillier"):
     logger.info("Current task type is {}.".format(eva_type))
 
     # 读取注册数据
-    data = ph.dataset.read(dataset_key=data_key).df_data
+    # data = ph.dataset.read(dataset_key=data_key).df_data
     # data = ph.dataset.read(dataset_key='train_hetero_xgb_host').df_data
-    # data = pd.read_csv(
-    #     '/primihub/data/FL/hetero_xgb/train/epsilon_normalized.t.host',
-    #     header=0)
-    # data = data.iloc[:, 550:]
+    data = pd.read_csv(
+        '/primihub/data/FL/hetero_xgb/train/epsilon_normalized.t.host',
+        header=0)
+    data = data.iloc[:, 550:]
 
     # y = data.pop('Class').values
 
@@ -1973,15 +1973,15 @@ def xgb_guest_logic(cry_pri="paillier"):
     host_ip, host_port = node_addr_map[host_nodes[0]].split(":")
 
     proxy_client_host = ClientChannelProxy(host_ip, host_port, "host")
-    data = ph.dataset.read(dataset_key=data_key).df_data
+    # data = ph.dataset.read(dataset_key=data_key).df_data
 
     if 'id' in data.columns:
         data.pop('id')
     # data = ph.dataset.read(dataset_key='train_hetero_xgb_guest').df_data
-    # data = pd.read_csv(
-    #     '/primihub/data/FL/hetero_xgb/train/epsilon_normalized.t.guest',
-    #     header=0)
-    # data = data.iloc[:, :450]
+    data = pd.read_csv(
+        '/primihub/data/FL/hetero_xgb/train/epsilon_normalized.t.guest',
+        header=0)
+    data = data.iloc[:, :450]
 
     X_guest = data
     # guest_log = open('/app/guest_log', 'w+')
