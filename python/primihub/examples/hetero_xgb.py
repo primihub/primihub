049c9bdda2467f40c482a07e8dcb98613930d020
xusong <xu15701157125@163.com>
2023-01-03 14:39:41 +0800

    fix(song.xu):add ray initizalization

--- python/primihub/examples/hetero_xgb.py
+++ python/primihub/examples/hetero_xgb.py
@@ -81,6 +81,8 @@ file_handler = FLFileHandler(jb_id=1,
                              format=FORMAT)
 fl_file_log = file_handler.set_format()
 
+ray.init("ray://172.21.3.126:10001")
+
 
 def goss_sample(df_g, top_rate=0.2, other_rate=0.2):
     df_g_cp = abs(df_g.copy())
@@ -953,8 +955,43 @@ class XGB_GUEST_EN:
             #     print(tmp_task.get())
 
         if self.encrypted:
-            res = list(
-                self.grouppools.map(lambda a, v: a.groupby.remote(v), groups))
+            if len(groups) > 20:
+                mid = int(len(groups) / 2)
+                groups1 = groups[:mid]
+                groups2 = groups[mid:]
+
+                res1 = list(
+                    self.grouppools.map(lambda a, v: a.groupby.remote(v),
+                                        groups1))
+
+                with self.cli1:
+                    if self.merge_gh:
+                        res2 = [
+                            tmp_group._aggregate_on(
+                                PallierSum,
+                                on=['g'],
+                                ignore_nulls=True,
+                                pub_key=self.pub,
+                                add_actors=self.paillier_add_actors).to_pandas(
+                                ) for tmp_group in groups2
+                        ]
+
+                    else:
+                        res2 = [
+                            tmp_group._aggregate_on(
+                                PallierSum,
+                                on=['g', 'h'],
+                                ignore_nulls=True,
+                                pub_key=self.pub,
+                                add_actors=self.paillier_add_actors).to_pandas(
+                                ) for tmp_group in groups2
+                        ]
+
+                res = res1 + res2
+            else:
+                res = list(
+                    self.grouppools.map(lambda a, v: a.groupby.remote(v),
+                                        groups))
 
             # if len(groups) > 20:
             #     mid = int(len(groups) / 2)
@@ -2302,12 +2339,12 @@ def xgb_guest_logic(cry_pri="paillier"):
     xgb_guest.paillier_add_actors = paillier_add_actors
     xgb_guest.grouppools = grouppools
 
-    # cli1 = ray.init("ray://172.21.3.126:10001", allow_multiple=True)
+    cli1 = ray.init("ray://172.21.3.126:10001", allow_multiple=True)
 
     # xgb_guest.channel.send(b'recved pub')
     lookup_table_sum = {}
     xgb_guest.lookup_table = {}
-    # xgb_guest.cli1 = cli1
+    xgb_guest.cli1 = cli1
 
     xgb_guest.fit(X_guest, lookup_table_sum)
 
