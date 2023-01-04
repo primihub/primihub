c0cdf8e8504b45f04573cd4ea09377829d0440b5
zhangjianning <zhangjianning@primihub.com>
2023-01-04 15:02:30 +0800

    fix build_local script

--- build_local.sh
+++ build_local.sh
@@ -9,7 +9,7 @@ fi
 
 bash pre_build.sh
 
-bazel build --config=linux --define cpu=amd64 --define microsoft-apsi=true :node :cli :opt_paillier_c2py
+bazel build --config=linux --define cpu=amd64 --define microsoft-apsi=true :node :cli :opt_paillier_c2py :linkcontext
 
 if [ $? -ne 0 ]; then
     echo "Build failed!!!"
