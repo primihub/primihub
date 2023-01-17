02250eb025c37146eac1428fbc0ab373200422d8
Xuefeng Xu <xuxf100@qq.com>
2023-01-17 10:22:59 +0800

    Add feature selection and data preprocessing in inference

--- python/primihub/FL/model/logistic_regression/homo_lr_infer.py
+++ python/primihub/FL/model/logistic_regression/homo_lr_infer.py
@@ -5,38 +5,46 @@ import pandas as pd
 import primihub as ph
 from primihub import dataset
 import logging
-from primihub.FL.model.logistic_regression.homo_lr import read_data
 
-#def read_data(dataset_key):
-#    x = ph.dataset.read(dataset_key).df_data
-#    if 'id' in x.columns:
-#        x.pop('id')
-#    y = x.pop('y').values
-#    x = x.to_numpy()
-#    return x, y
+def read_data(dataset_key, feature_names):
+    x = ph.dataset.read(dataset_key).df_data
+    if 'id' in x.columns:
+        x.pop('id')
+    y = x.pop('y').values
+    if feature_names != None:
+        x = x[feature_names]
+    x = x.to_numpy()
+    return x, y
 
 
 def sigmoid(x):
     return 1.0 / (1.0 + np.exp(-x))
 
 
-def predict(model, x): 
-    prob = sigmoid(x.dot(model[1:]) + model[0])
+def predict(theta, x): 
+    prob = sigmoid(x.dot(theta[1:]) + theta[0])
     return (prob > 0.5).astype('int')
 
 
 class ModelInfer:
     def __init__(self, model_path, input_file, output_path, model_type="Homo-LR") -> None:
-        model_f = open(model_path, 'rb')
-        self.model = np.array(pickle.load(model_f))
-        x, y = read_data(input_file)
+        with open(model_path, 'rb') as model_f:
+            self.model = pickle.load(model_f)
+        x, y = read_data(input_file, self.model['feature_names'])
+
+        # data preprocessing
+        # minmaxscaler
+        data_max = self.model['data_max']
+        data_min = self.model['data_min']
+        x = (x - data_min) / (data_max - data_min)
+
         self.x = x
         self.type = model_type
         self.out = output_path
 
     def infer(self):
         if self.type == "Homo-LR":
-            preds = predict(self.model, self.x)
+            preds = predict(self.model['theta'], self.x)
 
         dir_name = os.path.dirname(self.out)
 
@@ -46,15 +54,12 @@ class ModelInfer:
         pd.DataFrame(preds).to_csv(self.out, index=False)
         return preds
 
-
 infer_data = ['test_homo_lr']
 
-
-@ph.context.function(role='host', protocol='lr-infer', datasets=infer_data, port='9020', task_type="lr-regression-infer")
-def run_infer():
+def run_infer(party_name):
     logging.info("Start machine learning inferring.")
     predict_file_path = ph.context.Context.get_predict_file_path()
-    model_file_path = ph.context.Context.get_model_file_path()
+    model_file_path = ph.context.Context.get_model_file_path() + "." + party_name
 
     logging.info("Model file path is: {}".format(model_file_path))
     logging.info("Predict file path is: {}".format(predict_file_path))
@@ -65,3 +70,8 @@ def run_infer():
 
     logging.info(
         "Finish machine learning inferring. And the result is {}".format(preds))
+
+
+@ph.context.function(role='host', protocol='lr-infer', datasets=infer_data, port='9020', task_type="lr-regression-infer")
+def run_infer_host():
+    run_infer("host")
