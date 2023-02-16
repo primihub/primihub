8ba89ae11ccabe3874dcf37d7c5a1b328baca6f0
xusong <xu15701157125@163.com>
2023-02-16 16:19:00 +0800

    feat(song.xu):feat(song.xu):add momentum method for heter lr

--- python/primihub/FL/model/logistic_regression/hetero_lr_guest.py
+++ python/primihub/FL/model/logistic_regression/hetero_lr_guest.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from primihub.FL.model.logistic_regression.hetero_lr_base import HeteroLrBase, batch_yield, trucate_geometric_thres
 
 
@@ -15,11 +16,15 @@ class HeterLrGuest(HeteroLrBase):
                  loss_type='log',
                  random_state=2023,
                  guest_channel=None,
-                 add_noise=True):
+                 add_noise=True,
+                 n_iter_no_change=5,
+                 momentum=0.7):
         super().__init__(learning_rate, alpha, epochs, penalty, batch_size,
                          optimal_method, update_type, loss_type, random_state)
         self.channel = guest_channel
         self.add_noise = add_noise
+        self.n_iter_no_change = n_iter_no_change
+        self.momentum = momentum
 
     def predict(self, x):
         guest_part = np.dot(x, self.theta)
@@ -42,10 +47,26 @@ class HeterLrGuest(HeteroLrBase):
         return grad
 
     def batch_gd(self, x):
-        for batch_x, _ in batch_yield(x, x, self.batch_size):
+        ids = self.channel.recv('ids')
+        shuff_x = x[ids]
+        for batch_x, _ in batch_yield(shuff_x, shuff_x, self.batch_size):
             self.predict(batch_x)
             grad = self.gradient(batch_x)
             self.theta -= self.learning_rate * grad
+
+    def momentum_grad(self, x):
+        ids = self.channel.recv('ids')
+        shuff_x = x[ids]
+        for batch_x, _ in batch_yield(shuff_x, shuff_x, self.batch_size):
+            self.predict(batch_x)
+            grad = self.gradient(batch_x)
+
+            update = self.momentum * self.prev_grad - self.learning_rate * grad * (
+                1 - self.momentum)
+
+            self.theta += update
+
+            self.prev_grad = grad
 
     def simple_gd(self, x):
         self.predict(x)
@@ -53,22 +74,40 @@ class HeterLrGuest(HeteroLrBase):
         self.theta -= self.learning_rate * grad
 
     def fit(self, x):
+        if isinstance(x, pd.DataFrame):
+            x = x.values
         if self.batch_size < 0:
             self.batch_size = x.shape[0]
 
         self.theta = np.zeros(x.shape[1])
+
         for i in range(self.epochs):
             self.learning_rate = self.channel.recv("learning_rate")
 
             if self.optimal_method == "simple":
                 self.simple_gd(x)
 
+            elif self.optimal_method == "momentum":
+                self.momentum_grad(x)
+
             else:
                 self.batch_gd(x)
 
             self.predict(x)
+            print("current params: ", i, self.theta)
 
-            is_converged = self.channel.recv('is_converged')
+            current_stats = self.channel.recv('current_stats')
+
+            best_iter_changed = current_stats['best_iter_changed']
+            is_converged = current_stats['is_converged']
+
+            if best_iter_changed:
+                # best_theta = self.theta
+                # care about numpy.copy
+                best_theta = np.copy(self.theta)
 
             if is_converged:
                 break
+
+        self.theta = best_theta
+        print("best theta: ", best_theta)
--- python/primihub/FL/model/logistic_regression/hetero_lr_host.py
+++ python/primihub/FL/model/logistic_regression/hetero_lr_host.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from primihub.FL.model.logistic_regression.hetero_lr_base import HeteroLrBase, batch_yield, dloss, trucate_geometric_thres
 
 
@@ -16,12 +17,20 @@ class HeterLrHost(HeteroLrBase):
                  random_state=2023,
                  host_channel=None,
                  add_noise=True,
-                 tol=0.001):
+                 tol=0.001,
+                 momentum=0.7,
+                 n_iter_no_change=5):
         super().__init__(learning_rate, alpha, epochs, penalty, batch_size,
                          optimal_method, update_type, loss_type, random_state)
         self.channel = host_channel
         self.add_noise = add_noise
         self.tol = tol
+        self.momentum = momentum
+        self.prev_grad = 0
+        typw = np.sqrt(1.0 / np.sqrt(self.alpha))
+        initial_eta0 = typw / max(1.0, dloss(-typw, 1.0))
+        self.optimal_init = 1.0 / (initial_eta0 * self.alpha)
+        self.n_iter_no_change = n_iter_no_change
 
     def add_intercept(self, x):
         intercept = np.ones((x.shape[0], 1))
@@ -48,12 +57,8 @@ class HeterLrHost(HeteroLrBase):
         if type == "sqrt":
             self.learning_rate /= np.sqrt(current_epoch + 1)
         else:
-            typw = np.sqrt(1.0 / np.sqrt(self.alpha))
-            initial_eta0 = typw / max(1.0, dloss(-typw, 1.0))
-            optimal_init = 1.0 / (initial_eta0 * self.alpha)
-
             self.learning_rate = 1.0 / (self.alpha *
-                                        (optimal_init + current_epoch + 1))
+                                        (self.optimal_init + current_epoch + 1))
 
     def gradient(self, x, y):
         h = self.sigmoid(self.predict_raw(x))
@@ -84,7 +89,11 @@ class HeterLrHost(HeteroLrBase):
         return grad
 
     def batch_gd(self, x, y):
-        for batch_x, bathc_y in batch_yield(x, y, self.batch_size):
+        ids = np.random.permutation(np.arange(len(y)))
+        self.channel.sender('ids', ids)
+        shuff_x = x[ids]
+        shuff_y = y[ids]
+        for batch_x, bathc_y in batch_yield(shuff_x, shuff_y, self.batch_size):
             grad = self.gradient(batch_x, bathc_y)
             self.theta -= self.learning_rate * grad
 
@@ -92,14 +101,36 @@ class HeterLrHost(HeteroLrBase):
         grad = self.gradient(x, y)
         self.theta -= self.learning_rate * grad
 
+    def momentum_grad(self, x, y):
+        ids = np.random.permutation(np.arange(len(y)))
+        self.channel.sender('ids', ids)
+        shuff_x = x[ids]
+        shuff_y = y[ids]
+        for batch_x, bathc_y in batch_yield(shuff_x, shuff_y, self.batch_size):
+            grad = self.gradient(batch_x, bathc_y)
+
+            update = self.momentum * self.prev_grad - self.learning_rate * grad * (
+                1 - self.momentum)
+
+            self.theta += update
+
+            self.prev_grad = grad
+
+        pass
+
     def fit(self, x, y):
+        if isinstance(x, pd.DataFrame):
+            x = x.values
         x = self.add_intercept(x)
         if self.batch_size < 0:
             self.batch_size = x.shape[0]
 
         self.theta = np.zeros(x.shape[1])
-        pre_loss = None
+        total_loss = []
+        best_loss = None
+        n_iter_nochange = 0
         is_converged = False
+        best_iter_changed = False
         for i in range(self.epochs):
             self.update_lr(i, type=self.update_type)
             self.channel.sender("learning_rate", self.learning_rate)
@@ -107,28 +138,47 @@ class HeterLrHost(HeteroLrBase):
             if self.optimal_method == "simple":
                 self.simple_gd(x, y)
 
+            elif self.optimal_method == "momentum":
+                self.momentum_grad(x, y)
+
             else:
                 self.batch_gd(x, y)
 
             y_hat = self.predict_raw(x)
             cur_loss = self.loss(y_hat, y)
+            total_loss.append(cur_loss)
+            print("cur_loss :", i, cur_loss)
 
-            if pre_loss is None:
-                pre_loss = cur_loss
+            if best_loss is None or best_loss > cur_loss:
+                best_iter_changed = True
+                best_loss = cur_loss
+                best_params = self.theta
+                best_iter = i
+                n_iter_nochange = 0
+                best_y = y_hat
+                best_acc = sum(((self.sigmoid(best_y) > 0.5).astype('int')
+                                == y).astype('int')) / len(y)
 
             else:
-                loss_diff = abs(pre_loss - cur_loss)
-                pre_loss = cur_loss
+                n_iter_nochange += 1
+                best_iter_changed = False
 
-                if loss_diff < self.tol:
-                    is_converged = True
+            if n_iter_nochange > self.n_iter_no_change:
+                is_converged = True
 
-            self.channel.sender('is_converged', is_converged)
-
+            self.channel.sender(
+                'current_stats', {
+                    "best_iter_changed": best_iter_changed,
+                    'is_converged': is_converged
+                })
+            
             if is_converged:
                 break
 
-        pred_prob = self.sigmoid(y_hat)
-        preds = (pred_prob > 0.5).astype('int')
-        acc = sum((preds == y).astype('int')) / len(y)
-        print("acc: ", acc)
+        converged_acc = best_acc
+        converged_loss = best_loss
+        converged_iter = best_iter
+        self.theta = best_params
+
+        print("converged status: ", converged_iter, converged_acc,
+              converged_loss)
