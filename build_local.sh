43e624a36e855dd60a41936f32d776b070787388
fuxingbit <zjning95@126.com>
2023-03-03 16:38:16 +0800

    update build_local (#326)
    Co-authored-by: zhangjianning <zhangjianning@primihub.com>

--- build_local.sh
+++ build_local.sh
@@ -2,9 +2,15 @@
 # When using this script, please make sure that the python version of the local machine is 3.8
 
 if [ ! -n "$1" ] ; then
-    tag=`date +%F-%H-%M-%S`
+    TAG=`date +%F-%H-%M-%S`
 else
-    tag=$1
+    TAG=$1
+fi
+
+if [ ! -n "$2"] ; then
+    IMAGE_NAME="primihub/primihub-node"
+else
+    IMAGE_NAME=$2
 fi
 
 bash pre_build.sh
@@ -25,9 +31,9 @@ if [ ! -d "$BASE_DIR" ]; then
     exit
 fi
 
-IMAGE_NAME="primihub/primihub-node"
-
-sed -i "12c ARG TARGET_PATH=$BASE_DIR" Dockerfile.local
+key_word="ARG TARGET_PATH="
+row_num=$(sed -n "/${key_word}/=" Dockerfile.local)
+sed -i "${row_num}s#.*#ARG TARGET_PATH="${BASE_DIR}"#" Dockerfile.local
 
 rm -rf $BASE_DIR/python $BASE_DIR/config
 rm -f $BASE_DIR/Dockerfile.local
@@ -43,4 +49,4 @@ cp -r ./src $BASE_DIR/
 cd $BASE_DIR
 find ./ -name "_objs" > .dockerignore
 
-docker build -t $IMAGE_NAME:$tag . -f Dockerfile.local
+docker build -t $IMAGE_NAME:$TAG . -f Dockerfile.local
