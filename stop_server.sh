d412840c2599c069eb638acbea014f0dd0b101ba
phoenix20162016 <cuibo20062006@163.com>
2023-03-24 19:20:07 +0800

    build release package (#363)

---
+++ stop_server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -x
+if [ -z "${USER}" ]; then
+  pids=$(ps -ef |grep "bazel-bin/node" | grep -v grep |awk '{print $2}')
+else
+  pids=$(ps -ef |grep "bazel-bin/node" |grep ${USER} | grep -v grep |awk '{print $2}')
+fi
+echo $pids
+if [[ -n "${pids}" ]]; then
+  kill -9 ${pids}
+fi
