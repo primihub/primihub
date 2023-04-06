2ba4c678fe0c66277d7efb9b5c1186b3b9556a05
Bye-legumes <zhilong.chen@mail.mcgill.ca>
2023-04-06 16:15:58 +0800

    fix: fix executor

--- python/primihub/executor.py
+++ python/primihub/executor.py
@@ -15,9 +15,10 @@
  """
 import traceback
 from cloudpickle import loads
+import json
 from primihub.context import Context
 from primihub.utils.logger_util import logger
-
+from primihub.client.ph_grpc.src.primihub.protos import worker_pb2
 class Executor:
     '''
     Excute the py file. Note the Context is passed
@@ -28,4 +29,24 @@ class Executor:
 
     @staticmethod
     def execute_py():
-        print(Context.message)
+        PushTaskRequest = worker_pb2.PushTaskRequest()
+        PushTaskRequest.ParseFromString(Context.message)
+        task = PushTaskRequest.task
+
+
+
+        name = task.name
+        params = task.params
+        print(f"params: {params}")
+        code = task.code
+        for role in code:
+            code[role] = loads(code[role])
+        print(f"code : {code}")
+        task_info = task.task_info
+        party_datasets = task.party_datasets
+        print(f"party_datasets: {party_datasets}")
+        party_access_info = task.party_access_info
+        print(f"party_access_info : {party_access_info}s")
+
+
+        
