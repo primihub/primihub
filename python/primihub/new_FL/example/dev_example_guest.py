b6c4e9dba11e6f554d41c0fcde2704517667df75
Bye-legumes <zhilong.chen@mail.mcgill.ca>
2023-04-10 10:26:03 +0800

    fix : fix host guest file

--- python/primihub/new_FL/example/dev_example_guest.py
+++ python/primihub/new_FL/example/dev_example_guest.py
@@ -2,8 +2,7 @@ import pandas as pd
 class Dev_example_guest:
     @staticmethod
     def run(task_parameter, party_access_info):
-        df = pd.DataFrame([1,2,3])
-        df.to_csv("/home/primihub/czl/res.csv")
+        print(task_parameter)
     
 
     def train(self, X, y):
