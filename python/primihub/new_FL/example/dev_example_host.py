b3abe36c9188dcce8bfe767e2878752d81278e2e
Bye-legumes <zhilong.chen@mail.mcgill.ca>
2023-04-11 11:18:43 +0800

    fix: fix executor

--- python/primihub/new_FL/example/dev_example_host.py
+++ python/primihub/new_FL/example/dev_example_host.py
@@ -2,8 +2,8 @@ import pandas as pd
 class Dev_example_host:
     @staticmethod
     def run(task_parameter, party_access_info):
-        print(task_parameter)
-        print(pd.read_csv(task_parameter['data']['X']['data_path']))
+        X = eval(task_parameter['X'])
+        print(pd.read_csv(X['data_path']))
     
 
     def train(self, X, y):
