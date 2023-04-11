96fc1beaa8fb4a8e8a91c2c2abe5daedf8b067de
Bye-legumes <zhilong.chen@mail.mcgill.ca>
2023-04-11 12:24:35 +0800

    fix: fix host

--- python/primihub/new_FL/example/dev_example_host.py
+++ python/primihub/new_FL/example/dev_example_host.py
@@ -2,7 +2,7 @@ import pandas as pd
 class Dev_example_host:
     @staticmethod
     def run(task_parameter, party_access_info):
-        X = eval(task_parameter['X'])
+        X = eval(task_parameter['data']['X'])
         print(pd.read_csv(X['data_path']))
     
 
