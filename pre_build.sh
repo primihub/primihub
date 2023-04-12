a8f0ed3e58b965f84e07600fef1a285dac07ef00
root <pikachu1995@126.com>
2023-04-12 09:33:53 +0800

    add copt for python include

--- pre_build.sh
+++ pre_build.sh
@@ -23,11 +23,20 @@ if ! command -v python$U_V1.$U_V2-config >/dev/null 2>&1; then
   echo "please install python$U_V1.$U_V2-dev"
   exit
 fi
+if command -v yum &>/dev/null; then
+CONFIG=`python$U_V1.$U_V2-config --ldflags` && NEWLINE="[\"${CONFIG}\"]"
+else
 CONFIG=`python$U_V1.$U_V2-config --ldflags` && NEWLINE="[\"${CONFIG}\"] + [\"-lpython$U_V1.$U_V2\"]"
+fi
+PYTHON_INCLUDES=`python$U_V1.$U_V2-config --includes` && INCLUDE_PATH="[\"${PYTHON_INCLUDES}\"]"
 # Compatible with MacOS
 sed -e "s|PLACEHOLDER-PYTHON3.X-CONFIG|${NEWLINE}|g" BUILD.bazel > BUILD.bazel.tmp && mv BUILD.bazel.tmp BUILD.bazel
+sed -e "s|PLACEHOLDER-PYTHON3.X-INCLUDES|${INCLUDE_PATH}|g" BUILD.bazel > BUILD.bazel.tmp && mv BUILD.bazel.tmp BUILD.bazel
 echo "done"
-
+pushd src/primihub/pybind_warpper
+sed -e "s|PLACEHOLDER-PYTHON3.X-CONFIG|${NEWLINE}|g" BUILD > BUILD.bazel.tmp && mv BUILD.bazel.tmp BUILD
+sed -e "s|PLACEHOLDER-PYTHON3.X-INCLUDES|${INCLUDE_PATH}|g" BUILD > BUILD.bazel.tmp && mv BUILD.bazel.tmp BUILD
+popd
 if [ ! -d "localdb" ]; then
     mkdir localdb
 fi
