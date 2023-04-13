4a66a6bccdb14ce82cb147da14956197ad564f08
Bye-legumes <zhilong.chen@mail.mcgill.ca>
2023-04-13 15:07:51 +0800

    feat: network for the newsdk

--- python/primihub/new_FL/example/dev_example_guest.py
+++ python/primihub/new_FL/example/dev_example_guest.py
@@ -1,8 +1,13 @@
 import pandas as pd
+from primihub.new_FL.utils.net_work import GrpcServer
 class Dev_example_guest:
     @staticmethod
     def run(task_parameter, party_access_info):
         print(task_parameter)
+        print(party_access_info)
+
+        server = GrpcServer('Alice','Bob', party_access_info, task_parameter['task_info'])
+        server.recv('abc')
     
 
     def train(self, X, y):
