ab116665b4e8945043ec7845dc37358d83d83745
Bye-legumes <zhilong.chen@mail.mcgill.ca>
2023-04-13 17:09:25 +0800

    fix: fix network

--- python/primihub/new_FL/utils/net_work.py
+++ python/primihub/new_FL/utils/net_work.py
@@ -4,27 +4,7 @@ import linkcontext
 def Node(ip, port, use_tls, nodename = "default"):
     return linkcontext.Node(ip, port, use_tls, nodename)
 
-def init_link_context(local_party_access_info, task_info):
 
-    use_tls = local_party_access_info.use_tls
-    link_context = linkcontext.LinkFactory.createLinkContext(linkcontext.LinkMode.GRPC)
-    print(task_info['task_id'], task_info['job_id'], task_info['request_id'])
-    link_context.setTaskInfo(task_info['task_id'], task_info['job_id'], task_info['request_id'])
-    return link_context
-    #not used at this moment
-    '''
-    if use_tls:
-        # load certificate
-        cert_confg = config_node.get("certificate", {})
-        if cert_confg:
-            ca_file = cert_confg["root_ca"]
-            key_file = cert_confg["key"]
-            cert_file = cert_confg["cert"]
-            logger.debug(ca_file)
-            logger.debug(key_file)
-            logger.debug(cert_file)
-            self.link_context.initCertificate(ca_file, key_file, cert_file)
-    '''
 class GrpcServer:
 
     def __init__(self, local_party, remote_party, party_access_info, task_info) -> None:
@@ -37,8 +17,10 @@ class GrpcServer:
         print(local_ip, int(local_port))
         recv_session = Node(local_ip, int(local_port), False, local_party)
 
-        self.send_channel = init_link_context(party_access_info[remote_party], task_info).getChannel(send_session)
-        self.recv_channel = init_link_context(party_access_info[local_party],task_info).getChannel(recv_session)
+
+
+        self.send_channel = self.init_link_context(party_access_info[remote_party], task_info).getChannel(send_session)
+        self.recv_channel = self.init_link_context(party_access_info[local_party],task_info).getChannel(recv_session)
 
     def sender(self, key, val):
         print("Start to send")
@@ -50,3 +32,24 @@ class GrpcServer:
         recv_val = self.recv_channel.recv(key)
         print("End receive")
         return pickle.loads(recv_val)
+    
+    def init_link_context(self,local_party_access_info, task_info):
+        use_tls = local_party_access_info.use_tls
+        link_context = linkcontext.LinkFactory.createLinkContext(linkcontext.LinkMode.GRPC)
+        print(task_info['task_id'], task_info['job_id'], task_info['request_id'])
+        link_context.setTaskInfo(task_info['task_id'], task_info['job_id'], task_info['request_id'])
+        return link_context
+        #not used at this moment
+        '''
+        if use_tls:
+            # load certificate
+            cert_confg = config_node.get("certificate", {})
+            if cert_confg:
+                ca_file = cert_confg["root_ca"]
+                key_file = cert_confg["key"]
+                cert_file = cert_confg["cert"]
+                logger.debug(ca_file)
+                logger.debug(key_file)
+                logger.debug(cert_file)
+                self.link_context.initCertificate(ca_file, key_file, cert_file)
+    '''
