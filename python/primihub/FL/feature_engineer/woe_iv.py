d24cc15463e32f8b0317ac8ce1cfec5fe172e6e7
xusong <xu15701157125@163.com>
2023-04-17 16:48:09 +0800

    fix(song.xu):reset paramters for pandas.qcut

--- python/primihub/FL/feature_engineer/woe_iv.py
+++ python/primihub/FL/feature_engineer/woe_iv.py
@@ -46,17 +46,16 @@ class IVBase:
                     # bucket-binning according to pre-set bins
 
                     if self.bin_type == "equal_size":
-                        bin_bucket = pd.qcut(
-                            self.data[cur_feature],
-                            cur_feature_bins,
-                            labels=np.arange(len(cur_feature_bins) - 1),
-                            duplicates='drop')
+                        bin_bucket = pd.qcut(self.data[cur_feature],
+                                             cur_feature_bins,
+                                             labels=False,
+                                             duplicates='drop')
 
                     else:
-                        bin_bucket = pd.cut(
-                            self.data[cur_feature],
-                            bins=cur_feature_bins,
-                            labels=np.arange(len(cur_feature_bins) - 1))
+                        bin_bucket = pd.cut(self.data[cur_feature],
+                                            bins=cur_feature_bins,
+                                            labels=np.arange(
+                                                len(cur_feature_bins)))
 
                     self.data[cur_feature + "_cate"] = bin_bucket
 
@@ -66,7 +65,7 @@ class IVBase:
                     if self.bin_type == "equal_size":
                         bin_bucket = pd.qcut(self.data[cur_feature],
                                              self.bin_num,
-                                             labels=np.arange(self.bin_num - 1),
+                                             labels=False,
                                              duplicates='drop')
                     else:
                         bin_bucket = pd.cut(self.data[cur_feature],
