a5bc54f7e6457e78d1a0961052c20c706dd80595
Bye-legumes <zhilong.chen@mail.mcgill.ca>
2023-04-19 18:36:07 +0800

    add ChatGlm

---
+++ python/primihub/new_FL/algorithm/example/ChatGlm_host.py
@@ -0,0 +1,24 @@
+from primihub.new_FL.algorithm.utils.base import BaseModel
+import os
+class ChatGlmHost(BaseModel):
+    def __init__(self, task_parameter, party_access_info):
+        super().__init__(task_parameter, party_access_info)
+        self.task_parameter = task_parameter
+        self.party_access_info = party_access_info
+    
+    def run(self):
+        if self.task_parameter['process'] == 'train':
+            self.train()
+    
+
+    def train(self):
+        task_parameter = self.task_parameter
+        path = "/home/primihub/czl/ChatGLM-6B-Med"
+        cmd = path+ "/ptuning/train.sh"
+        print(f"cmd is {cmd}")
+        import time
+        time.sleep(100)
+        os.cmd(cmd)
+    
+    def predict(self):
+        pass
