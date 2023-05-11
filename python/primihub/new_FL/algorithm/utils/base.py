5c29ec3311deb82f7153c3c9270dc5f152427c62
xujiangyu <47973791+xujiangyu@users.noreply.github.com>
2023-05-11 15:14:42 +0800

    feat: set params in base model (#443)
    * feat(song.xu): set params in base model

--- python/primihub/new_FL/algorithm/utils/base.py
+++ python/primihub/new_FL/algorithm/utils/base.py
@@ -6,6 +6,10 @@ class BaseModel(metaclass=ABCMeta):
 
     def __init__(self, **kwargs):
         self.kwargs = kwargs
+        self.common_params = self.kwargs['common_params']
+        self.role_params = self.kwargs['role_params']
+        self.node_info = self.kwargs['node_info']
+        self.other_params = self.kwargs['other_params']
 
     @abstractmethod
     def get_summary(self) -> Dict:
