c2d2eb4198773fd14f9325861159040f2c1f4139
Xuefeng Xu <xuxf100@qq.com>
2023-05-29 11:07:49 +0800

    adapt HFL chatglm to new python sdk

--- python/primihub/new_FL/algorithm/example/ChatGlm_client.py
+++ python/primihub/new_FL/algorithm/example/ChatGlm_client.py
@@ -1,39 +1,32 @@
 from primihub.new_FL.algorithm.utils.base import BaseModel
-from primihub.new_FL.algorithm.utils.net_work import GrpcServers
+from primihub.new_FL.algorithm.utils.net_work import GrpcClient
 
 import os
 import time
 
+
 class ChatGlmClient(BaseModel):
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.role_params = self.kwargs['role_params']
-        self.other_params = self.kwargs['other_params']
-        self.node_info = self.kwargs['node_info']
-        self.common_params = self.kwargs['common_params']
 
-    
     def run(self):
         if self.common_params['process'] == 'train':
             self.train()
-    
 
     def train(self):
         role_params = self.role_params
 
         num_examples = self.role_params['num_examples']
 
-        #send num_sampes        
+        #send num_sampes
+        remote_party = self.roles[self.role_params['others_role']]
+        self.channel = GrpcClient(local_party=self.role_params['self_name'],
+                                  remote_party=remote_party,
+                                  node_info=self.node_info,
+                                  task_info=self.task_info)
 
-        self.channel = GrpcServers(local_role=self.other_params.party_name,
-                                   remote_roles=self.role_params['neighbors'],
-                                   party_info=self.node_info,
-                                   task_info=self.other_params.task_info)
-
-
-    
-        self.channel.sender('num_examples', num_examples)
-        
+        self.channel.send('num_examples', num_examples)
 
         path = role_params['path']
         os.chdir(path)
@@ -45,7 +38,8 @@ class ChatGlmClient(BaseModel):
         validation_file = role_params['validation_file']
         prompt_column = role_params['prompt_column']
         response_column = role_params["response_column"]
-        history_column = role_params['history_column'] if 'history_column' in role_params else None
+        history_column = role_params[
+            'history_column'] if 'history_column' in role_params else None
         model_name_or_path = role_params['model_name_or_path']
         output_dir = role_params['output_dir']
         ptuning_checkpoint = f"{output_dir}/checkpoint-{train_iter}"
@@ -73,25 +67,28 @@ class ChatGlmClient(BaseModel):
                         --learning_rate $LR \
                         --pre_seq_len $PRE_SEQ_LEN \
                         --quantization_bit 4 "
+
             if history_column != None:
                 cmd += f"  --history_column {history_column}"
-            if i!=0:
+            if i != 0:
                 cmd += f"  --ptuning_checkpoint {ptuning_checkpoint}"
             print(f"cmd is {cmd}")
             os.system(cmd)
-            time.sleep(5) #for save
+            time.sleep(5)  #for save
             import torch
-            prefix_state_dict = torch.load(os.path.join(path+"/"+ptuning_checkpoint, "pytorch_model.bin"))
+            prefix_state_dict = torch.load(
+                os.path.join(path + "/" + ptuning_checkpoint,
+                             "pytorch_model.bin"))
             del torch
-            
-            self.channel.sender(f'client_res_{i}', prefix_state_dict)
+
+            self.channel.send(f'client_res_{i}', prefix_state_dict)
             res = self.channel.recv(f'server_res_{i}')
             import torch
-            torch.save(res, os.path.join(path+"/"+ptuning_checkpoint, "pytorch_model.bin"))
+            torch.save(
+                res,
+                os.path.join(path + "/" + ptuning_checkpoint,
+                             "pytorch_model.bin"))
             del torch
-
-
-
 
     def predict(self):
         pass
--- python/primihub/new_FL/algorithm/example/ChatGlm_server.py
+++ python/primihub/new_FL/algorithm/example/ChatGlm_server.py
@@ -1,44 +1,41 @@
 from primihub.new_FL.algorithm.utils.base import BaseModel
-from primihub.new_FL.algorithm.utils.net_work import GrpcServers
+from primihub.new_FL.algorithm.utils.net_work import MultiGrpcClients
 
 import os
 import time
+
+
 class ChatGlmServer(BaseModel):
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.role_params = self.kwargs['role_params']
-        self.other_params = self.kwargs['other_params']
-        self.node_info = self.kwargs['node_info']
-        self.common_params = self.kwargs['common_params']
-    
+
     def run(self):
         if self.common_params['process'] == 'train':
             self.train()
-    
 
     def train(self):
+        remote_parties = self.roles[self.role_params['others_role']]
+        self.channel = MultiGrpcClients(
+            local_party=self.role_params['self_name'],
+            remote_parties=remote_parties,
+            node_info=self.node_info,
+            task_info=self.task_info)
 
-        self.channel = GrpcServers(local_role=self.other_params.party_name,
-                                   remote_roles=self.role_params['neighbors'],
-                                   party_info=self.node_info,
-                                   task_info=self.other_params.task_info)
-
-        num_examples_weights = self.channel.recv('num_examples')
+        num_examples_weights = self.channel.recv_all('num_examples')
 
         total_sample = sum(num_examples_weights)
-        weights = [k/total_sample for k in num_examples_weights]
+        weights = [k / total_sample for k in num_examples_weights]
         #receive weights
-        for i in range(self.task_parameter['aggration_iter']):
-            received_dict = self.channel.recv(f'client_res_{i}')
+        for i in range(self.common_params['aggration_iter']):
+            received_dict = self.channel.recv_all(f'client_res_{i}')
             new_prefix_state_dict = {}
-            for prefix_state_dict in received_dict:
+            for idx, prefix_state_dict in enumerate(received_dict):
                 for k, v in prefix_state_dict.items():
                     if k.startswith("transformer.prefix_encoder."):
-                        new_prefix_state_dict[k] = v*weights[j]
+                        new_prefix_state_dict[k] = v * weights[idx]
 
-
-            self.channel.sender(f'server_res_{i}', new_prefix_state_dict)
-        
+            self.channel.send_all(f'server_res_{i}', new_prefix_state_dict)
 
     def predict(self):
         pass
