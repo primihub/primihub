4727be1f7d1968b2d69f3fe4b2686cbdd839e206
Xuefeng Xu <xuxf100@qq.com>
2023-06-07 16:21:20 +0800

    fix bug in chatglm (#500)
    * fix bug in chatglm
    
    * fix key error

--- python/primihub/FL/algorithm/chatglm/load_model.py
+++ python/primihub/FL/algorithm/chatglm/load_model.py
@@ -1,33 +1,32 @@
 import os
 import platform
 import signal
-
-import torch
-
-import os
 import torch
 from transformers import AutoConfig, AutoModel, AutoTokenizer
 
 # 载入Tokenizer
-tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b",
+                                          trust_remote_code=True)
 
 model_name_or_path = "/home/primihub/czl/chatglm-6b-int4"
 pre_seq_len = 128
 quantization_bit = 4
 CHECKPOINT_PATH = "/home/primihub/czl/ChatGLM-6B-Med/ptuning/output/Alice_result/checkpoint-3"
 
-
-
-config = AutoConfig.from_pretrained(model_name_or_path, trust_remote_code=True, pre_seq_len=128)
-model = AutoModel.from_pretrained(model_name_or_path, config=config, trust_remote_code=True)
+config = AutoConfig.from_pretrained(model_name_or_path,
+                                    trust_remote_code=True,
+                                    pre_seq_len=128)
+model = AutoModel.from_pretrained(model_name_or_path,
+                                  config=config,
+                                  trust_remote_code=True)
 
 #Load the model 1
-prefix_state_dict = torch.load(os.path.join(CHECKPOINT_PATH, "pytorch_model.bin"))
+prefix_state_dict = torch.load(
+    os.path.join(CHECKPOINT_PATH, "pytorch_model.bin"))
 new_prefix_state_dict = {}
 for k, v in prefix_state_dict.items():
     if k.startswith("transformer.prefix_encoder."):
         new_prefix_state_dict[k[len("transformer.prefix_encoder."):]] = v
-
 
 model.transformer.prefix_encoder.load_state_dict(new_prefix_state_dict)
 
@@ -35,7 +34,6 @@ model = model.quantize(4)
 model = model.half().cuda()
 model.transformer.prefix_encoder.float()
 model = model.eval()
-
 
 os_name = platform.system()
 clear_command = 'cls' if os_name == 'Windows' else 'clear'
@@ -69,7 +67,9 @@ def main():
             print("欢迎使用 ChatGLM-6B 模型，输入内容即可进行对话，clear 清空对话历史，stop 终止程序")
             continue
         count = 0
-        for response, history in model.stream_chat(tokenizer, query, history=history):
+        for response, history in model.stream_chat(tokenizer,
+                                                   query,
+                                                   history=history):
             if stop_stream:
                 stop_stream = False
                 break
