1452457050e405b0913822583cfb7723149dc7ff
phoenix20162016 <cuibo20062006@163.com>
2023-06-28 13:47:03 +0800

    add

--- src/primihub/task/semantic/pir_util.cc
+++ src/primihub/task/semantic/pir_util.cc
@@ -1,15 +1,6 @@
 // Copyright [2023] <Primihub>
 #include "src/primihub/task/semantic/pir_util.h"
 
-#include <glog/logging.h>
-#include <algorithm>
-#include <cctype>
-#include <fstream>
-#include <sstream>
-#include <utility>
-
-using namespace apsi;
-
 namespace primihub::pir::util {
 
 
--- src/primihub/task/semantic/pir_util.h
+++ src/primihub/task/semantic/pir_util.h
@@ -1,17 +1,7 @@
 //  Copyright [2023] <Primihub>
 #ifndef SRC_PRIMIHUB_TASK_SEMANTIC_PIR_UTIL_H_
 #define SRC_PRIMIHUB_TASK_SEMANTIC_PIR_UTIL_H_
-// STD
-#include <string>
-#include <unordered_map>
-#include <unordered_set>
-#include <utility>
 #include <variant>
-#include <vector>
-#include <map>
-
-#include "src/primihub/common/common.h"
-
 // APSI
 #include "apsi/item.h"
 #include "apsi/psi_params.h"
