bda87a4c5314d66134ec9d6ab84f3b687e371342
phoenix20162016 <cuibo20062006@163.com>
2023-06-28 13:44:16 +0800

    remove

--- src/primihub/task/semantic/pir_util.h
+++ src/primihub/task/semantic/pir_util.h
@@ -19,33 +19,13 @@
 
 namespace primihub::pir::util {
 
-retcode throw_if_file_invalid(const std::string& file_name);
 class CSVReader {
  public:
   using UnlabeledData = std::vector<apsi::Item>;
   using LabeledData = std::vector<std::pair<apsi::Item, apsi::Label>>;
   using DBData = std::variant<UnlabeledData, LabeledData>;
-
   CSVReader() = default;
 
-  explicit CSVReader(const std::string& file_name);
-
-  std::pair<DBData, std::vector<std::string>> read(std::istream& stream);
-
-  std::pair<DBData, std::vector<std::string>> read();
-  std::pair<DBData, std::vector<std::string>> read(std::atomic<bool>* stop_flag);
-
- protected:
-    auto pre_process(std::istream& stream) -> std::pair<std::map<std::string, std::string>, bool>;
-    std::pair<bool, bool> process_line(const std::string& line,
-                                      std::string& item,
-                                      std::string& label);
- private:
-    std::string file_name_;
-    std::pair<bool, bool> process_line(const std::string &line,
-                                      std::string &orig_item,
-                                      apsi::Item &item,
-                                      apsi::Label &label) const;
 };  // class CSVReader
 }  // namespace primihub::pir::util
 #endif  // SRC_PRIMIHUB_TASK_SEMANTIC_PIR_UTIL_H_
