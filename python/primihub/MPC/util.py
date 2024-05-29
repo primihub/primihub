8dd03ba7b55bcb07cf87491808269d79e6743074
default <default>
2024-05-29 14:11:22 +0800

    add cert info is tls enabled for stop task

--- python/primihub/MPC/util.py
+++ python/primihub/MPC/util.py
@@ -2,5 +2,10 @@ import ph_secure_lib as ph_slib
 from primihub.context import Context
 
 def stop_auxiliary_party():
-  mpc_executor = ph_slib.MPCExecutor(Context.message, "ABY3", "", "", "")
+  cert_config = Context.cert_config
+  root_ca_path = cert_config.get("root_ca_path", "")
+  key_path = cert_config.get("key_path", "")
+  cert_path = cert_config.get("cert_path", "")
+  mpc_executor = ph_slib.MPCExecutor(Context.message, "ABY3",
+                                      root_ca_path, key_path, cert_path)
   mpc_executor.stop_task()
